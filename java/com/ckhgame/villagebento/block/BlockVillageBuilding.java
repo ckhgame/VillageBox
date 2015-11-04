7be8de3070708abae970d410ba6adcfe569beef0
ckhgame <ckhgame@Gmail.com>
2015-11-04 11:44:25 -0700

    player message

--- java/com/ckhgame/villagebento/block/BlockVillageBuilding.java
+++ java/com/ckhgame/villagebento/block/BlockVillageBuilding.java
@@ -1,19 +1,17 @@
 package com.ckhgame.villagebento.block;
 
-import java.util.Random;
-
 import com.ckhgame.villagebento.Main;
 import com.ckhgame.villagebento.building.Building;
 import com.ckhgame.villagebento.data.DataVillageBento;
 import com.ckhgame.villagebento.data.helper.HelperDataVB;
 import com.ckhgame.villagebento.network.VBNetwork;
 import com.ckhgame.villagebento.network.message.MessageVillageOutlinesChanged;
+import com.ckhgame.villagebento.util.PlayerMsg;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.world.World;
 
 public class BlockVillageBuilding extends Block {
@@ -41,18 +39,24 @@ public class BlockVillageBuilding extends Block {
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z,EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
 		
-		if(world.isRemote || world.provider.dimensionId !=0) return true;
-		else{
-	
-			if(Building.build(world, player, x, y, z, this.buildingClass)){
-				//refresh village outlines
-				DataVillageBento dataVB = DataVillageBento.get(world);			
-				MessageVillageOutlinesChanged msg = new MessageVillageOutlinesChanged();
-				msg.listOutlines = HelperDataVB.getVillageOutlines(dataVB);			
-				VBNetwork.networkWrapper.sendToAll(msg);
+		if(!world.isRemote || world.provider.dimensionId !=0){
+			//the player must be very close to the block to use it
+			if(player.getDistance(x, y, z) < 1.5){
+				if(Building.build(world, player, x, y, z, this.buildingClass)){
+					//refresh village outlines
+					DataVillageBento dataVB = DataVillageBento.get(world);			
+					MessageVillageOutlinesChanged msg = new MessageVillageOutlinesChanged();
+					msg.listOutlines = HelperDataVB.getVillageOutlines(dataVB);			
+					VBNetwork.networkWrapper.sendToAll(msg);
+				}
+				else{
+					PlayerMsg.send(player, "Sorry, You can't build it here!");
+				}
+			} 
+			else{
+				PlayerMsg.send(player, "Please get closer to the block");
 			}
-			 
-			return true;
 		}	
+		return true;
 	}
 }
