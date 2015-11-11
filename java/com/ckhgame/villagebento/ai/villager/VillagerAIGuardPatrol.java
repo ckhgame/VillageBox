fa19e1f232374db08c6d98c305cc46d0e4cc2547
ckhgame <ckhgame@Gmail.com>
2015-11-10 21:45:37 -0700

    bug fix

--- java/com/ckhgame/villagebento/ai/villager/VillagerAIGuardPatrol.java
+++ java/com/ckhgame/villagebento/ai/villager/VillagerAIGuardPatrol.java
@@ -78,9 +78,29 @@ public class VillagerAIGuardPatrol extends EntityAIBase
      */
     public void startExecuting()
     {
+    	equipWeaponAndArmors(true);
         this.entity.getNavigator().tryMoveToXYZ(this.xPosition, this.yPosition, this.zPosition, ConfigVillager.VillagerMoveSpeed);
     }
     
-    
+    private void equipWeaponAndArmors(boolean equip){
+    	if(equip){
+    		if(this.entity.getEquipmentInSlot(0) == null){
+        		this.entity.setCurrentItemOrArmor(0, new ItemStack(Items.iron_sword));
+            	this.entity.setCurrentItemOrArmor(1, new ItemStack(Items.iron_boots));
+            	this.entity.setCurrentItemOrArmor(2, new ItemStack(Items.iron_chestplate));
+            	this.entity.setCurrentItemOrArmor(3, new ItemStack(Items.iron_helmet));
+            	this.entity.setCurrentItemOrArmor(4, new ItemStack(Items.iron_leggings));
+    		}
+    	}
+    	else{
+    		if(this.entity.getEquipmentInSlot(0) != null){
+    			this.entity.setCurrentItemOrArmor(0, null);
+        		this.entity.setCurrentItemOrArmor(1, null);
+        		this.entity.setCurrentItemOrArmor(2, null);
+        		this.entity.setCurrentItemOrArmor(3, null);
+        		this.entity.setCurrentItemOrArmor(4, null);
+    		}
+    	}
+    }
     
 }
