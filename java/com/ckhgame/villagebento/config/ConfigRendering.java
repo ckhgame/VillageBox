4385e3456786293c334be90c65b7f99509f15e95
ckhgame <ckhgame@Gmail.com>
2015-11-13 05:07:14 -0700

    new effect for village outlines

--- java/com/ckhgame/villagebento/config/ConfigRendering.java
+++ java/com/ckhgame/villagebento/config/ConfigRendering.java
@@ -1,8 +1,8 @@
 package com.ckhgame.villagebento.config;
 
 public class ConfigRendering {
-	public static final int[] ColorVillageBuildingOutlines = {255,0,255,255};
-	public static final int[] ColorVillageOutlines = {255,0,0,255};
+	public static final int[] ColorVillageBuildingOutlines = {49,134,173,255};
+	public static final int[] ColorVillageOutlines = {150,201,80,255};
 	
 	public static final double[] OffsetVillageOutlines = {0.1,0.1,0.1};
 }
--- java/com/ckhgame/villagebento/event/EventRenderVillageOutlines.java
+++ java/com/ckhgame/villagebento/event/EventRenderVillageOutlines.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.lwjgl.opengl.GL11;
 
+import com.ckhgame.villagebento.Main;
 import com.ckhgame.villagebento.config.ConfigBuilding;
 import com.ckhgame.villagebento.data.DataBuilding;
 import com.ckhgame.villagebento.data.DataVillage;
@@ -15,6 +16,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 
 public class EventRenderVillageOutlines {
@@ -38,6 +40,10 @@ public class EventRenderVillageOutlines {
 	
 	private double px,py,pz;
 	private ArrayList<BoxWithColor> villageOutlines = null;
+	
+	private static ResourceLocation outlineTexture = new ResourceLocation(Main.MODID + ":" + "textures/effects/village/outlinefill.png");
+	
+	private double coordOffet = 0;
 	
 	public void setVillageOutlines(ArrayList<BoxWithColor> villageOutlines){ //if set vbData to null, no outlines will be display
 		this.villageOutlines = villageOutlines;
@@ -64,6 +70,10 @@ public class EventRenderVillageOutlines {
 			py = p.lastTickPosY + (p.posY - p.lastTickPosY) * (double) event.partialTicks;
 			pz = p.lastTickPosZ + (p.posZ - p.lastTickPosZ) * (double) event.partialTicks;
 			
+			coordOffet += event.partialTicks * 0.01D;
+			if(coordOffet > 1.0D)
+				coordOffet -= 1.0D; 
+			
 			renderOutlines();
 		}
     }
@@ -79,13 +89,70 @@ private void renderOutlines() {
         for(BoxWithColor b:villageOutlines)
         	renderOutline(b);
         
-       
-
         GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
         GL11.glEnable(GL11.GL_CULL_FACE);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glDisable(GL11.GL_BLEND);
+        
+        //----
+        
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_CULL_FACE);
+        
+        for(BoxWithColor b:villageOutlines)
+        	renderOutlineFill(b);
+        
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glDisable(GL11.GL_BLEND);
     }
+
+private void renderOutlineFill(BoxWithColor b){
+	 
+	Tessellator tessellator = Tessellator.instance;
+    
+	double minx = b.minX - px - 0.1D;
+	double miny = b.minY - py;
+	double minz = b.minZ - pz - 0.1D;
+	double maxx = b.maxX - px + 0.1D;
+	double maxy = b.maxY - py;
+	double maxz = b.maxZ - pz + 0.1D;
+	double co = coordOffet;//= maxx == minx ? 0:coordOffet * 10.0D /(maxx - minx);
+	
+	Minecraft.getMinecraft().renderEngine.bindTexture(outlineTexture);
+	
+	
+	tessellator.startDrawingQuads();
+	tessellator.setColorRGBA(b.R, b.G, b.B, b.A);
+	tessellator.addVertexWithUV(minx, miny, minz, 1 + co, 1); //Bottom left texture
+	tessellator.addVertexWithUV(minx, maxy, minz, 1 + co, 0); //Top left
+	tessellator.addVertexWithUV(maxx, maxy, minz, 0 + co, 0); //Top right
+	tessellator.addVertexWithUV(maxx, miny, minz, 0 + co, 1); //Bottom right
+	tessellator.draw();
+	
+	tessellator.startDrawingQuads();
+	tessellator.setColorRGBA(b.R, b.G, b.B, b.A);
+	tessellator.addVertexWithUV(minx, miny, maxz, 0 + co, 1); //Bottom left texture
+	tessellator.addVertexWithUV(minx, maxy, maxz, 0 + co, 0); //Top left
+	tessellator.addVertexWithUV(maxx, maxy, maxz, 1 + co, 0); //Top right
+	tessellator.addVertexWithUV(maxx, miny, maxz, 1 + co, 1); //Bottom right
+	tessellator.draw();
+	
+	tessellator.startDrawingQuads();
+	tessellator.setColorRGBA(b.R, b.G, b.B, b.A);
+	tessellator.addVertexWithUV(minx, miny, minz, 0 + co, 1); //Bottom left texture
+	tessellator.addVertexWithUV(minx, maxy, minz, 0 + co, 0); //Top left
+	tessellator.addVertexWithUV(minx, maxy, maxz, 1 + co, 0); //Top right
+	tessellator.addVertexWithUV(minx, miny, maxz, 1 + co, 1); //Bottom right
+	tessellator.draw();
+	
+	tessellator.startDrawingQuads();
+	tessellator.setColorRGBA(b.R, b.G, b.B, b.A);
+	tessellator.addVertexWithUV(maxx, miny, minz, 1 + co, 1); //Bottom left texture
+	tessellator.addVertexWithUV(maxx, maxy, minz, 1 + co, 0); //Top left
+	tessellator.addVertexWithUV(maxx, maxy, maxz, 0 + co, 0); //Top right
+	tessellator.addVertexWithUV(maxx, miny, maxz, 0 + co, 1); //Bottom right
+	tessellator.draw();
+}
 
 private void renderOutline(BoxWithColor b) {
     
@@ -115,7 +182,7 @@ private void renderOutline(BoxWithColor b) {
     tessellator.addVertex(b.minX, b.maxY, b.maxZ);
     tessellator.draw();
  	*/
-    
+	
 	tessellator.startDrawing(GL11.GL_LINE_LOOP);
     tessellator.setColorRGBA(b.R, b.G, b.B, b.A);
     tessellator.addVertex(minx, maxy, maxz);
