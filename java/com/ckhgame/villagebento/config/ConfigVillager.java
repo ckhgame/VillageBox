25a7a4c9591ecdcee16e33d6e467699ec69d8f85
ckhgame <ckhgame@Gmail.com>
2015-11-19 02:29:10 -0700

    updated trading exp

--- java/com/ckhgame/villagebento/config/ConfigVillager.java
+++ java/com/ckhgame/villagebento/config/ConfigVillager.java
@@ -2,7 +2,7 @@ package com.ckhgame.villagebento.config;
 
 public class ConfigVillager {
 	public static final int VillagerRevivingDurationByDay = 2; //how long the village will be revived after death (on day)
-	public static final int TradingExp = 150;	//how much exp the villager will get for each trading action
+	public static final int TradingExp = 3;	//how much exp the villager will get for each trading action
 	public static final int WorkExpPerHour = 1;	//how much exp the villager will get for working one hour
 	public static final int WorkOutputExpirationHours = 48;	//if the work out hasn't been took more than 2 days, it will automatically disappear!
 	
