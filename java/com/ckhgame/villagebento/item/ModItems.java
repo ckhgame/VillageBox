772f0649fd5b2dfb53513a3770468ec643dcac21
wilusy <wilusyzeng@gmail.com>
2015-11-19 02:48:28 -0700

    Added Seeds, Corps and some Food

--- java/com/ckhgame/villagebento/item/ModItems.java
+++ java/com/ckhgame/villagebento/item/ModItems.java
@@ -28,6 +28,41 @@ public class ModItems {
 	public static Item itemCowFood;
 	public static Item itemMilkBottle;
 	public static Item itemVillageMilk;
+	public static Item itemGuaze;
+	
+	//seeds
+	public static Item itemCabbageSeed;
+	public static Item itemGrapeSeed;
+	public static Item itemCornSeed;
+	public static Item itemChiliSeed;
+	public static Item itemRiceSeed;
+	public static Item itemSoybeanSeed;
+	
+	//crops
+	public static Item itemCabbage;
+	public static Item itemChili;
+	public static Item itemGrapes;
+	public static Item itemRiceplant;
+	public static Item itemSoybeans;
+	public static Item itemCorn;
+	
+	//food
+	public static Item itemAppleCandy;
+	public static Item itemBoiledEgg;
+	public static Item itemCarrotCake;
+	public static Item itemCheese;
+	public static Item itemFries;
+	public static Item itemHambuger;
+	public static Item itemMayonaise;
+	public static Item itemMelonIce;
+	public static Item itemPorkRamen;
+	public static Item itemRice;
+	public static Item itemRiceFlour;
+	public static Item itemSalt;
+	public static Item itemUdon;
+	public static Item itemWheatFlour;
+	
+	
 	
 	public static final void init() {
         GameRegistry.registerItem(itemVillageStick = new ItemVillageStick(), "ItemVillageStick");
@@ -52,6 +87,46 @@ public class ModItems {
         GameRegistry.registerItem(itemCowFood = new ItemVB("ItemCowFood",null,CreativeTabs.tabMisc),"ItemCowFood");
         GameRegistry.registerItem(itemMilkBottle = new ItemVB("ItemMilkBottle",null,CreativeTabs.tabTools),"ItemMilkBottle");
         GameRegistry.registerItem(itemVillageMilk = new ItemVB("ItemVillageMilk",null,CreativeTabs.tabMaterials),"ItemVillageMilk");
+        
+        GameRegistry.registerItem(itemGuaze = new ItemVB("itemGuaze",null,CreativeTabs.tabMaterials),"ItemGuaze");
+        
+        
 
+        
+        //Seeds
+        GameRegistry.registerItem(itemCabbageSeed = new ItemVB("ItemCabbageSeed",null,CreativeTabs.tabMisc),"ItemCabbageSeed");
+        GameRegistry.registerItem(itemGrapeSeed = new ItemVB("ItemGrapeSeed",null,CreativeTabs.tabMisc),"ItemGrapeSeed");
+        GameRegistry.registerItem(itemCornSeed = new ItemVB("ItemCornSeed",null,CreativeTabs.tabMisc),"ItemCornSeed");
+        GameRegistry.registerItem(itemChiliSeed = new ItemVB("ItemChiliSeed",null,CreativeTabs.tabMisc),"ItemChiliSeed");
+        GameRegistry.registerItem(itemRiceSeed = new ItemVB("ItemRiceSeed",null,CreativeTabs.tabMisc),"ItemRiceSeed");
+        GameRegistry.registerItem(itemSoybeanSeed = new ItemVB("ItemSoybeanSeed",null,CreativeTabs.tabMisc),"ItemSoybeanSeed");
+        
+        //Crops
+        GameRegistry.registerItem(itemCabbage = new ItemVB("ItemCabbage",null,CreativeTabs.tabMisc),"ItemCabbage");
+        GameRegistry.registerItem(itemChili = new ItemVB("ItemChili",null,CreativeTabs.tabMisc),"ItemChili");
+        GameRegistry.registerItem(itemGrapes = new ItemVB("ItemGrapes",null,CreativeTabs.tabMisc),"ItemGrapes");
+        GameRegistry.registerItem(itemRiceplant = new ItemVB("ItemRiceplant",null,CreativeTabs.tabMisc),"ItemRiceplant");
+        GameRegistry.registerItem(itemSoybeans = new ItemVB("ItemSoybeans",null,CreativeTabs.tabMisc),"ItemSoybeans");
+        GameRegistry.registerItem(itemCorn = new ItemVB("ItemCorn",null,CreativeTabs.tabMisc),"ItemCorn");
+        
+        //Food
+        GameRegistry.registerItem(itemAppleCandy = new ItemVB("ItemAppleCandy",null,CreativeTabs.tabFood),"ItemAppleCandy");
+        GameRegistry.registerItem(itemBoiledEgg = new ItemVB("ItemBoiledEgg",null,CreativeTabs.tabFood),"ItemBoiledEgg");
+        GameRegistry.registerItem(itemCarrotCake = new ItemVB("ItemCarrotCake",null,CreativeTabs.tabFood),"ItemCarrotCake");
+        GameRegistry.registerItem(itemCheese = new ItemVB("ItemCheese",null,CreativeTabs.tabFood),"ItemCheese");
+        GameRegistry.registerItem(itemFries = new ItemVB("ItemFries",null,CreativeTabs.tabFood),"ItemFries");
+        GameRegistry.registerItem(itemHambuger = new ItemVB("ItemHambuger",null,CreativeTabs.tabFood),"ItemHambuger");
+        GameRegistry.registerItem(itemMayonaise = new ItemVB("ItemMayonaise",null,CreativeTabs.tabFood),"ItemMayonaise");
+        GameRegistry.registerItem(itemMelonIce = new ItemVB("ItemMelonIce",null,CreativeTabs.tabFood),"ItemMelonIce");
+        GameRegistry.registerItem(itemPorkRamen = new ItemVB("ItemPorkRamen",null,CreativeTabs.tabFood),"ItemPorkRamen");
+        GameRegistry.registerItem(itemRice = new ItemVB("ItemRice",null,CreativeTabs.tabFood),"ItemRice");
+        GameRegistry.registerItem(itemRiceFlour = new ItemVB("ItemRiceFlour",null,CreativeTabs.tabFood),"ItemRiceFlour");
+        GameRegistry.registerItem(itemSalt = new ItemVB("ItemSalt",null,CreativeTabs.tabFood),"ItemSalt");
+        GameRegistry.registerItem(itemUdon = new ItemVB("ItemUdon",null,CreativeTabs.tabFood),"ItemUdon");
+        GameRegistry.registerItem(itemWheatFlour = new ItemVB("ItemWheatFlour",null,CreativeTabs.tabFood),"ItemWheatFlour");
+
+
+        
+        
     }
 }
