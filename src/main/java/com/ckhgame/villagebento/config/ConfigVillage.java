15f4a09b496f599dc490d286ff10a63180f3ce35
ckhgame <ckhgame@Gmail.com>
2016-01-09 21:43:37 -0700

    changed village config

--- src/main/java/com/ckhgame/villagebento/config/ConfigVillage.java
+++ src/main/java/com/ckhgame/villagebento/config/ConfigVillage.java
@@ -1,9 +1,9 @@
 package com.ckhgame.villagebento.config;
 
 public class ConfigVillage {
-	public static final int VillageBoundaryExtensionHorizontal = 8;
+	public static final int VillageBoundaryExtensionHorizontal = 9;
 	public static final int VillageBoundaryExtensionVertical = 2;
-	public static final int NewVillageSizeRequirment = 15;
+	public static final int NewVillageSizeRequirment = 20;
 	
 	public static final int FireProhibitDistance = 3;	//a distance near the village building. The player can not use flint_and_stone or fire_charger near the filed
 	
