4010409dcdc2461b4c5d74225ed68395564e4a33
ckhgame <ckhgame@gmail.com>
2016-03-22 16:58:48 -0600

    changed version to 0.2

--- src/main/java/ckhbox/villagebox/VillageBoxMod.java
+++ src/main/java/ckhbox/villagebox/VillageBoxMod.java
@@ -14,7 +14,7 @@ public class VillageBoxMod {
 
     public static final String MODID = "villagebox";
     public static final String MODNAME = "Village Box";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.2";
 
     @Instance
     public static VillageBoxMod instance = new VillageBoxMod();
