4e8b8a5c07283efba11012bac413b6a11b41bc03
ckhgame <ckhgame@gmail.com>
2016-04-07 14:52:12 -0600

    updated home page for guidebook

--- src/main/java/ckhbox/villagebox/client/gui/guidebook/page/PageHome.java
+++ src/main/java/ckhbox/villagebox/client/gui/guidebook/page/PageHome.java
@@ -16,16 +16,18 @@ public class PageHome extends Page{
 
 	@Override
 	public void onInit() {
-		this.addLink(new LinkText(this,"Items","itemlist=0",null),false);
-		this.addLink(new LinkText(this,"Professions","prolist=0",null),false);
-		this.addLink(new LinkText(this,"Beginner","beginner=",null),false);
+		int top = this.guiGuideBook.getContentTop();
+		this.addLink(new LinkText(this,"> Items","itemlist=0",null),top + 80,1);
+		this.addLink(new LinkText(this,"> Professions","prolist=0",null),top + 94,1);
+		this.addLink(new LinkText(this,"> Beginner","beginner=",null),top + 108,1);
+		this.addLink(new LinkText(this,"> Feedback","feedback=",null),top + 122,1);
 	}
 
 	@Override
 	public void onDrawScreen(int mouseX, int mouseY) {
 		super.onDrawScreen(mouseX, mouseY);
 		int x = this.guiGuideBook.getContentLeft() + this.guiGuideBook.getContentWidth() / 2 - 25;
-		int y = this.guiGuideBook.getContentTop() + 30;
+		int y = this.guiGuideBook.getContentTop() + 20;
 		GlStateManager.disableLighting();
 		this.guiGuideBook.drawTexture(x,y, 176, 0, 50, 50);
 	}
