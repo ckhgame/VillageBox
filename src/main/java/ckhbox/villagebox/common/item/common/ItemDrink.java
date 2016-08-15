439607c498fa7cb95b9cd7ff63e979beab5e7e1d
ckhgame <ckhgame@gmail.com>
2016-08-14 23:44:22 -0700

    fixed the drinking effect

--- src/main/java/ckhbox/villagebox/common/item/common/ItemDrink.java
+++ src/main/java/ckhbox/villagebox/common/item/common/ItemDrink.java
@@ -3,6 +3,7 @@ package ckhbox.villagebox.common.item.common;
 import ckhbox.villagebox.common.item.ModItems;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
+import net.minecraft.init.PotionTypes;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
@@ -29,7 +30,7 @@ public class ItemDrink extends ItemFood{
 	@Override
 	protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
 		super.onFoodEaten(stack, worldIn, player);
-		player.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS,this.drunkticks));
+		player.addPotionEffect(new PotionEffect(MobEffects.NAUSEA,this.drunkticks));
 	}
 	
 	
